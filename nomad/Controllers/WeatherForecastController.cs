04b4b4c98846d6f6968a7e82781a58ce86b52c4b
camilovegabarrios <camilovb@gmail.com>
2021-12-02 17:10:11 -0300

    * .gitignore: initial commit
    * nomad.sln:
    * Program.cs:
    * Startup.cs:
    * nomad.csproj:
    * appsettings.json:
    * WeatherForecast.cs:
    * appsettings.Development.json:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ nomad/Controllers/WeatherForecastController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using Newtonsoft.Json;
+using nomad.Models;
+using MongoDB.Driver;
+using nomad.Repositories;
+
+namespace nomad.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private ICityCollection db = new CityCollection();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            Random random = new Random();
+
+            Double num = random.NextDouble();
+            if(num < 0.1)
+            {
+                return BadRequest("How unfortunate! The API Request Failed");
+            }
+            else
+            {
+                List<weatherModel> result = new List<weatherModel>();
+                List<cityModel> cities = GetAllCities().Result;
+                for (int i = 0; i < cities.Count; i++)
+                {
+                    var client = new RestClient($"http://api.openweathermap.org/data/2.5/weather?q={(cities[i].city).Replace("'", string.Empty)}&appid=3e460f4d5fb539b95adac377c36396c9");
+                    client.Timeout = -1;
+                    var request = new RestRequest(Method.GET);
+                    IRestResponse response = client.Execute(request);
+                    result.Add(JsonConvert.DeserializeObject<weatherModel>(response.Content));
+                }
+
+                //return JsonConvert.DeserializeObject<weatherModel>(response.Content);
+                return Ok(result);
+            }
+            
+        }
+
+        public async Task<List<cityModel>> GetAllCities()
+        {
+            var response = await db.GetAllCities();
+            return (response);
+        }
+    }
+}
